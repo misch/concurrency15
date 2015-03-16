ccc08e1d023db7e2473b433cf52f5e0946c9da40
Michèle Wyss <michele_wyss@students.unibe.ch>
2015-03-16 17:03:26 +0100

    Startet assigment 2.
    Skeleton for filter algorithm.

---
+++ concurrency/src/assig2/Counter.java
@@ -0,0 +1,13 @@
+package assig2;
+
+/**
+ * Implements a simple counter that can be locked using the generalized Peterson algorithm (filter lock).
+ * @author Misch
+ * 
+ */
+public class Counter {
+	int value;
+	FilterLock lock;
+	
+	//TODO
+}
---
+++ concurrency/src/assig2/FilterLock.java
@@ -0,0 +1,32 @@
+package assig2;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+/**
+ * Implements the generalized Peterson algorithm to lock and unlock shared resources.
+ * @author Misch
+ *
+ */
+public class FilterLock {
+	private int threads;
+	
+	/** holds current level for each thread */
+	volatile AtomicIntegerArray level;
+	
+	/** holds identifier of last thread that entered one level */
+	volatile AtomicIntegerArray victim;
+	
+	public FilterLock(int threads){
+		this.threads = threads;
+		this.level = new AtomicIntegerArray(threads);
+		this.victim = new AtomicIntegerArray(threads);
+	}
+	
+	public void lock(){
+		//TODO
+	}
+	
+	public void unlock(){
+		//TODO
+	}
+}
